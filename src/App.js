5c44b2deee371b24c7710b9f2dd4f2429ffa9881
GodDevAI <goddevai@outlook.com>
2021-06-17 21:56:31 -0400

    message

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './styles/styles.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+    </div>
+  );
+}
+
+export default App;
